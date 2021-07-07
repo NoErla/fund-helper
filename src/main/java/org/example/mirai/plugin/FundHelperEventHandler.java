7357282b3860f663b3e2d3c519385c8ef661d2c3
noerla <34351184@qq.com>
2021-07-07 17:03:06 +0800

    修复排序问题

--- src/main/java/org/example/mirai/plugin/FundHelperEventHandler.java
+++ src/main/java/org/example/mirai/plugin/FundHelperEventHandler.java
@@ -11,10 +11,7 @@ import net.mamoe.mirai.event.events.GroupTempMessageEvent;
 import net.mamoe.mirai.event.events.MessageEvent;
 import org.example.mirai.plugin.netword.FundCrawler;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class FundHelperEventHandler extends SimpleListenerHost {
     /**
@@ -64,27 +61,27 @@ public class FundHelperEventHandler extends SimpleListenerHost {
                 System.out.println(data);
                 List<JSONObject> arrayLists = data.toList(JSONObject.class);
                 //排序
+                arrayLists.sort(Comparator.comparingDouble(x -> x.getDouble("changePercent")));
                 StringBuilder sb = new StringBuilder();
                 sb.append("领涨行业：").append("\n");
+                sb.append(arrayLists.get(arrayLists.size() - 1).getStr("name")).append(":").append(arrayLists.get(arrayLists.size() - 1).getStr("changePercent")).append("\n");
+                sb.append(arrayLists.get(arrayLists.size() - 2).getStr("name")).append(":").append(arrayLists.get(arrayLists.size() - 2).getStr("changePercent")).append("\n");
+                sb.append(arrayLists.get(arrayLists.size() - 3).getStr("name")).append(":").append(arrayLists.get(arrayLists.size() - 3).getStr("changePercent"));
+
+                sb.append("领跌行业：").append("\n");
 
                 sb.append(arrayLists.get(0).getStr("name")).append(":").append(arrayLists.get(0).getStr("changePercent")).append("\n");
                 sb.append(arrayLists.get(1).getStr("name")).append(":").append(arrayLists.get(1).getStr("changePercent")).append("\n");
                 sb.append(arrayLists.get(2).getStr("name")).append(":").append(arrayLists.get(2).getStr("changePercent")).append("\n");
-
-                sb.append("领跌行业：").append("\n");
-
-                sb.append(arrayLists.get(arrayLists.size() - 1).getStr("name")).append(":").append(arrayLists.get(arrayLists.size() - 1).getStr("changePercent")).append("\n");
-                sb.append(arrayLists.get(arrayLists.size() - 2).getStr("name")).append(":").append(arrayLists.get(arrayLists.size() - 2).getStr("changePercent")).append("\n");
-                sb.append(arrayLists.get(arrayLists.size() - 3).getStr("name")).append(":").append(arrayLists.get(arrayLists.size() - 3).getStr("changePercent"));
                 messageEvent.getSubject().sendMessage(sb.toString());
             } else {
                 String code = input.replace(".基金 ", "");
                 JSONObject fund = fundCrawler.getFund(code).getJSONArray("data").toList(JSONObject.class).get(0);
                 StringBuilder sb = new StringBuilder();
-                sb.append("基金编号: "+code).append("\n");
-                sb.append("基金名称："+fund.getStr("name")).append("\n");
-                sb.append("今日涨跌："+fund.getStr("dayGrowth")).append("\n");
-                sb.append("半年涨跌："+fund.getStr("lastSixMonthsGrowth")).append("\n");
+                sb.append("基金编号: ").append(code).append("\n");
+                sb.append("基金名称: ").append(fund.getStr("name")).append("\n");
+                sb.append("今日涨跌: ").append(fund.getStr("dayGrowth")).append("\n");
+                sb.append("半年涨跌: ").append(fund.getStr("lastSixMonthsGrowth")).append("\n");
                 messageEvent.getSubject().sendMessage(sb.toString());
             }
 
